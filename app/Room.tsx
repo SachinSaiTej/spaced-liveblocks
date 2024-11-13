b6baf8fa000d64d7c5a2ec00717dc5498f9a39f8
saitejakm2001 <saitejakm2001@gmail.com>
2024-11-13 07:16:13 +0000

    Initial Nextjs setup with liveblocks configured

---
+++ app/Room.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { ReactNode } from "react";
+import {
+  LiveblocksProvider,
+  RoomProvider,
+  ClientSideSuspense,
+} from "@liveblocks/react/suspense";
+
+export function Room({ children }: { children: ReactNode }) {
+  return (
+    <LiveblocksProvider publicApiKey={"pk_dev_nZ52xGqlmkRbReCIT9istMi2CX4XEenST2B-pI6sDRY2pq7eOSNfufH80J1pESgI"}>
+      <RoomProvider id="my-room">
+        <ClientSideSuspense fallback={<div>Loading…</div>}>
+          {children}
+        </ClientSideSuspense>
+      </RoomProvider>
+    </LiveblocksProvider>
+  );
+}
